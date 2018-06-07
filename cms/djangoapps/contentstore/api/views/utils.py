4b88e1f76b570511afdc1ae772159e6867649b67
Nimisha Asthagiri <nasthagiri@edx.org>
2018-06-07 13:12:20 -0400

    fixup! add utils.py file

---
+++ cms/djangoapps/contentstore/api/views/utils.py
@@ -0,0 +1,10 @@
+from openedx.core.djangoapps.util.forms import to_bool
+
+
+def get_bool_param(request, param_name, default):
+    param_value = request.query_params.get(param_name, None)
+    bool_value = to_bool(param_value)
+    if bool_value is None:
+        return default
+    else:
+        return bool_value
